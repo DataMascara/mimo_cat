2cc2542219dcf8f6d64392b3958798f6acc61ab1
katychuang <katychuang@gmail.com>
2020-12-05 18:26:01 -0500

    [View] Create login form

--- view/src/App.js
+++ view/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import login from './pages/login';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+        <div>
+          <Switch>
+              <Route exact path="/login" component={login}/>
+          </Switch>
+        </div>
+    </Router>
   );
 }
 
