d62fa7d99a33da82aa366b462d7f9e39468f0b34
katychuang <katychuang@gmail.com>
2020-12-05 20:07:22 -0500

    :pencil2: docs

--- history_install.sh
+++ history_install.sh
@@ -9,3 +9,30 @@ mkdir functions/api
 touch functions/api/media.js
 mkdir functions/util
 touch functions/util/admin.js
+
+# create more files for user auth
+touch functions/api/users.js
+touch functions/util/validators.js
+touch functions/util/config.js
+
+# for the file upload, these dependencies are required.
+# from within the function directory: 
+npm i firebase busboy 
+
+# front end 
+npm install -g create-react-app
+npx create-react-app view
+cd view
+
+# from within the view directory: 
+npm start
+npm i @material-ui/core@next @emotion/react @emotion/styled
+mkdir src/pages
+touch src/pages/login.js
+npm i  @material-ui/icons axios react-router-dom
+touch src/pages/signup.js
+touch src/pages/home.js
+
+mkdir src/util
+touch src/util/auth.js
+npm i dayjs
